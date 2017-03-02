bac1e0ff0aadd8e697299183c6a02f8c23f822b5
emersonlbr <emersonlbr@icloud.com>
2017-03-01 22:52:02 -0300

    adding a new method

---
+++ movideDB/methods.js
@@ -0,0 +1,12 @@
+var obj = {
+  name: "emerson",
+  age: 21,
+  isCool: false,
+  friends: ["himself", "his cat"],
+  //a function here!
+  add: function(x,y){
+    return x + y;
+  }
+}
+// calling the function
+obj.add(10,5); // same format as console.log();
