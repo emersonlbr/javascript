7e2b636ec32b714eb243ec829cad3fed18150ad6
emersonlbr <emersonlbr@icloud.com>
2017-03-10 18:42:01 -0300

    new files

---
+++ select/select.js
@@ -0,0 +1,19 @@
+// selecting the html
+var h1 = document.querySelector("h1");
+
+//manipulating the object
+h1.style.color = "green";
+
+
+// simple animation
+
+var body = document.querySelector("body"); //select
+var isBlue = false;
+
+setInterval(function(){
+  if (isBlue) {
+    body.style.background = "white";
+  } else {
+      body.style.background = "red";
+    }
+  }, 1000);
