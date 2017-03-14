b136bda86f579333e45d6bcafef6b7a8a33f62a4
emersonlbr <emersonlbr@icloud.com>
2017-03-14 20:57:20 -0300

    new files while at work

---
+++ dom/script.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function(e) {
+  document.querySelector('#test').innerHTML = 'learn';
+}, false);
+
+var p = document.getElementsByTagName("p");
+p[2].style.color = "green";
+
+
+console.log("this shit doesnt work");
+
+var body = document.getElementsByTagName("body");
+body[0].style.backgroundColor = "black";
+
+
+var x = document.getElementById("test");
+x.style.color = "pink";
+x.style.backgroundColor = "green";
+
+alert("welcome to my webpage!");
+
+
+console.log("okay now it does work");
+
+
+
+var button = document.getElementsByClassName("button");
+
+button.onclick = function(){
+  body[0].style.backgroundColor = "grey";
+
+}
+
+//changes the background color when click the button
