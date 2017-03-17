b9da1268e58b7c259bff4b9abbf3000b05a64677
emersonlbr <emersonlbr@icloud.com>
2017-03-16 22:30:07 -0300

    adding files

---
+++ dom/change.js
@@ -0,0 +1,9 @@
+var images = [
+  "http://www.google.com.br/images/1.jpg",
+  "http://www.google.com.br/images/2.jpg",
+   "http://www.google.com.br/images/3.jpg"];
+
+var num = 0;
+
+function next(){
+ var slider = document.getElementById("slider");
