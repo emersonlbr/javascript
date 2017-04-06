414eb39dbd3dc11a03e6de659a0f62e52a98492e
emersonlbr <emersonlbr@icloud.com>
2017-04-06 12:15:15 -0300

    jquery selectors

---
+++ jquery/index.js
@@ -0,0 +1,19 @@
+// testing if jQuery IS CONNECTED
+
+// if(jQuery){
+//   alert("eye");
+// } else {
+//   alert("nay");
+// }
+
+// selcting all the divs
+$("div").css("background", "purple");
+
+// selecting the class with .highlight
+$(".highlight").css("width", "200px");
+
+// selecting the div with the id "third" and give it a  border
+$("#third").css("border", "4px solid orange");
+
+// selecting only the first div
+$("div:first-of-type").css("color", "pink");
